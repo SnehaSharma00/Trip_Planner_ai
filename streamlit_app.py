bd9591fa7560adb164618a19324d2d8367250229
SnehaSharma00 <sanuwkuku@gmail.com>
2026-07-26 13:35:57 +0530

    name fixed

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ if submit_button and user_input.strip():
             markdown_content = f"""# 🌍 AI Travel Plan
 
             # **Generated:** {datetime.datetime.now().strftime('%Y-%m-%d at %H:%M')}  
-            # **Created by:** Atriyo's Travel Agent
+            # **Created by:** Sneha's Travel Agent
 
             ---
 
--- utils/save_to_document.py
+++ utils/save_to_document.py
@@ -10,7 +10,7 @@ def save_document(response_text: str, directory: str = "./output"):
     markdown_content = f"""# 🌍 AI Travel Plan
  
     # **Generated:** {datetime.datetime.now().strftime('%Y-%m-%d at %H:%M')}  
-    # **Created by:** Atriyo's Travel Agent
+    # **Created by:** Sneha's Travel Agent
 
     ---
 
